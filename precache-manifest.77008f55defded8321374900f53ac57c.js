8c2cf152431a18fe061b58f4a79ffb4f4c83b9d0
Lucas Ferreira <lucas0409lf@gmail.com>
2020-09-14 11:17:30 -0300

    Updates

---
+++ precache-manifest.77008f55defded8321374900f53ac57c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a5464c2b875268392b9c48a80fa24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27b938e30acb0a4704d",
+    "url": "/static/js/2.d69c452a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d69c452a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7835c088eb9bd1034b7b",
+    "url": "/static/js/main.c45204f8.chunk.js"
+  },
+  {
+    "revision": "91502cf749ccc806ccc8",
+    "url": "/static/js/runtime-main.37497303.js"
+  }
+]);
